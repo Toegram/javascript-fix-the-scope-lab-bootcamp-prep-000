35b35948bcbaec6f95ef70229bbe1b3f22742316
Anthony Turetsky <tturetsky@gmail.com>
2018-04-17 16:41:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var animal = 'dog'
+var animal = 'cat'
 
 function myAnimal() {
-  return animal
+  return animal;
 }
 
 function yourAnimal() {
@@ -12,7 +12,7 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  return n + 2;
 
   // Feel free to move things around!
   const two = 2
